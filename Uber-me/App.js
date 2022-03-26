b2e87dd157f5bd6b9007906fe48f02218a285a29
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-26 15:46:56 +0300

    install tailwind

--- Uber-me/App.js
+++ Uber-me/App.js
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Lets build uber and fuck off bitch
-        pullop
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+      
+      <HomeScreen/>
+     
+    
     </Provider>
   );
 }
