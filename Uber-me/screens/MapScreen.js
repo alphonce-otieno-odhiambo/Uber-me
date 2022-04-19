451fa3bf2c2f7cf40ef5cb19bccd6b1960df659b
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-04-19 13:24:33 +0300

    install react native maps

--- Uber-me/screens/MapScreen.js
+++ Uber-me/screens/MapScreen.js
@@ -1,13 +1,17 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react';
 import tw from "tailwind-react-native-classnames";
+import Map from '../components/Map';
 
 
 const MapScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-        <View style={tw`p-5`}>
-           
+        <View style={tw`h-1/2`}>
+           <Map/>
+        </View>
+        <View style={tw`h-1/2`}>
+
         </View>
       <Text style={tw`text-blue-500`}>I am the map screen</Text>
     </SafeAreaView>
